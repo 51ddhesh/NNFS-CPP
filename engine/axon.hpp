50bcc0cc855e9e6a34e92d20028bde589caabe93
siddhesh <siddheshbadnapurkar@gmail.com>
2025-05-25 14:33:32 +0530

    axon init

--- engine/axon.hpp
+++ engine/axon.hpp
@@ -1,27 +1,6 @@
 #ifndef __AXON_
 #define __AXON_
 
-#include <iostream>
-#include <vector>
-#include <utility> // For std::pair
 
-template <typename T>
-class numpy {
-public:
-    // Shape for 1D vector
-    std::pair<int, int> shape(const std::vector<T>& A) {
-        int x = A.size();
-        std::cerr << "(" << x << ",)" << std::endl;
-        return {x, 1}; 
-    }
-
-    // Shape for 2D vector
-    std::pair<int, int> shape(const std::vector<std::vector<T>>& A) {
-        int x = A.size();
-        int y = (x > 0) ? A[0].size() : 0; 
-        std::cerr << "(" << x << ", " << y << ")" << std::endl;
-        return {x, y};
-    }
-};
 
 #endif // __AXON_
